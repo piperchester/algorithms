9257d1bc85b72d3892dcfa8b87fb87fdcc5a3ffd
Piper Chester <piperchester@gmail.com>
2013-05-01 11:41:40 -0400

    Adding best and worst plists

--- algorithms/algorithms/DetailViewController.m
+++ algorithms/algorithms/DetailViewController.m
@@ -15,6 +15,8 @@
 
 @implementation DetailViewController{
     UITextView* averageText;
+    UITextView* bestText;
+    UITextView* worstText;
 }
 
 #pragma mark - Managing the detail item
@@ -42,22 +44,61 @@
         self.title = [self.detailItem description];
     }
     
+    NSString *averagePlistPath = [[NSBundle mainBundle] pathForResource:@"average" ofType:@"plist"];
+    NSDictionary *averageDictionary = [NSDictionary dictionaryWithContentsOfFile:averagePlistPath];
+    
+    NSString *bestPlistPath = [[NSBundle mainBundle] pathForResource:@"best" ofType:@"plist"];
+    NSDictionary *bestDictionary = [NSDictionary dictionaryWithContentsOfFile:bestPlistPath];
+    
+    NSString *worstPlistPath = [[NSBundle mainBundle] pathForResource:@"worst" ofType:@"plist"];
+    NSDictionary *worstDictionary = [NSDictionary dictionaryWithContentsOfFile:worstPlistPath];
+    
+   
+    bestText = [[UITextView alloc] initWithFrame:CGRectMake(100, 50, 140, 50)];
+    [bestText setBackgroundColor:[UIColor clearColor]];
+    [bestText setFont:[UIFont boldSystemFontOfSize:60]];
+    bestText.editable = NO;
+    bestText.scrollEnabled = NO;
+    bestText.allowsEditingTextAttributes = NO;
+    bestText.text = [bestDictionary objectForKey:@"bubble"];
+    [[self view] addSubview:bestText];
+    
+    
     averageText = [[UITextView alloc] initWithFrame:CGRectMake(100, 165, 140, 50)];
     [averageText setBackgroundColor:[UIColor clearColor]];
     [averageText setFont:[UIFont boldSystemFontOfSize:60]];
     averageText.editable = NO;
     averageText.scrollEnabled = NO;
     averageText.allowsEditingTextAttributes = NO;
-    
-    NSString *averagePlistPath = [[NSBundle mainBundle] pathForResource:@"average" ofType:@"plist"];
-    NSDictionary *averageDictionary = [NSDictionary dictionaryWithContentsOfFile:averagePlistPath];
     averageText.text = [averageDictionary objectForKey:@"bubble"];
     [[self view] addSubview:averageText];
     
-    CGRect frame = averageText.frame;
+    worstText = [[UITextView alloc] initWithFrame:CGRectMake(100, 300, 140, 0)];
+    [worstText setBackgroundColor:[UIColor clearColor]];
+    [worstText setFont:[UIFont boldSystemFontOfSize:60]];
+    worstText.editable = NO;
+    worstText.scrollEnabled = NO;
+    worstText.allowsEditingTextAttributes = NO;
+    worstText.text = [worstDictionary objectForKey:@"bubble"];
+    [[self view] addSubview:worstText];
+    
+    CGRect frame = bestText.frame;
+    frame.size.height = bestText.contentSize.height;
+    frame.size.width = bestText.contentSize.width;
+    bestText.frame = frame;
+    
+    frame = averageText.frame;
     frame.size.height = averageText.contentSize.height;
     frame.size.width = averageText.contentSize.width;
     averageText.frame = frame;
+    
+    
+    frame = worstText.frame;
+    frame.size.height = worstText.contentSize.height;
+    frame.size.width = worstText.contentSize.width;
+    worstText.frame = frame;
+    
+
 }
 
 - (void)viewDidLoad
